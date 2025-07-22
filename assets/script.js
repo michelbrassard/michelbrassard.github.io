c3d00ac503c7aa0b86ad705c24b172b0dec3b2b6
Michel Brassard <mb6149@rit.edu>
2025-07-22 10:49:19 +0200

    add mobile and desktop navigation

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,10 @@
-console.log("Loaded!")
+function handleOpenMobileNavigation() {
+  console.log("Navigation opened");
+  document.getElementById("overlay").style.width = "100%";
+  document.getElementById("overlay").style.opacity = "100%";
+}
+
+function handleCloseMobileNavigation() {
+  document.getElementById("overlay").style.width = "0%";
+  document.getElementById("overlay").style.opacity = "0%";
+}
