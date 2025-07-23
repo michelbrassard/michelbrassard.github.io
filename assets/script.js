92cabb95b4b6a0bda7a9b5edc94223b7eb46de95
Michel Brassard <mb6149@rit.edu>
2025-07-23 14:15:40 +0200

    fix mobile navigation

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,18 @@
-function handleOpenMobileNavigation() {
-  console.log("Navigation opened");
-  document.getElementById("overlay").style.width = "100%";
-  document.getElementById("overlay").style.opacity = "100%";
-}
+function toggleMobileNavigation() {
+  const overlay = document.getElementById("overlay");
+  const button = document.getElementById("mobile-navigation-hovering-button");
 
-function handleCloseMobileNavigation() {
-  document.getElementById("overlay").style.width = "0%";
-  document.getElementById("overlay").style.opacity = "0%";
+  const isOpen = overlay.style.width === "100%";
+
+  if (isOpen) {
+    overlay.style.width = "0%";
+    overlay.style.opacity = "0%";
+    button.style.border = "1px solid transparent";
+    button.style.boxShadow = "0px 4px 8px 2px rgba(0, 0, 0, 0.2)";
+  } else {
+    overlay.style.width = "100%";
+    overlay.style.opacity = "100%";
+    button.style.border = "1px solid var(--primary-color-light)";
+    button.style.boxShadow = "none";
+  }
 }
